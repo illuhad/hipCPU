56203427bd88ef2eeabaa3f2674094f86a1ec234
Federico Ficarelli <federico.ficarelli@gmail.com>
2020-04-01 19:27:01 +0200

    Fix data race on hipcpu::detail::async_queue member access
    Both the call to _enqueued_operations.empty() and the access to
    _continue are unsynced and a data race when:
    
    * T1 calls hipcpu::detail::async_queue::work
    * T2 calls hipcpu::detail::async_queue::operator()

--- include/hipCPU/hip/detail/queue.hpp
+++ include/hipCPU/hip/detail/queue.hpp
@@ -138,10 +138,14 @@ void async_queue::work()
   // The loop is executed as long as there are enqueued operations,
   // (_is_operation_pending) or we should wait for new operations
   // (_continue).
-  while(_continue || _enqueued_operations.size() > 0)
+  while(true)
   {
     {
       std::unique_lock<std::mutex> lock(_mutex);
+
+      // enclosing loop termination must be checked while holding
+      // a lock on _enqueued_operations (_continue is std::atomic<bool>)
+      if(!_continue && _enqueued_operations.size() <= 0) break;
 
       // Before going to sleep, wake up the other thread in case it is is waiting
       // for the queue to get empty
